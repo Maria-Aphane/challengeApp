7901f85becbf4359c9850f51cfaceeac787cd47c
Aphane Maria <mariaaphane17@gmail.com>
2018-08-03 10:27:51 +0200

    challenge app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ReactiveFormsModule } from '../../node_modules/@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
